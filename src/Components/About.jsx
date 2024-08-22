6e8c68e4b610a46ff3735ab9e332b51807826d2a
rbhdsks <rbhdsks@gmail.com>
2024-08-23 00:12:48 +0530

    Initial commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
     <div className="about-me">
       <h1 className="heading">About me</h1>
       <p>
-      Hi! I am Nitesh, a passionate technology enthusiast, currently pursuing 
+      Hello! I am Nitesh, a passionate technology enthusiast, currently pursuing 
       a B.Tech in Information Technology at the Indian Institute of Information Technology (IIIT) Allahabad. 
       I have a keen interest in the future of autonomous driving, surveillance systems, and industrial inspection. 
       My expertise lies in advanced machine learning frameworks like TensorFlow and the art of Generative Adversarial Networks (GANs),
